3fcd388068416ab2059b8ad0a3a541bc8d821604
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-24 21:25:02 +0200

    Create load.py

---
+++ core/load.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+from settings import folder_list
+import os
+
+
+class CoreLoad(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def load(self, ctx, types=None, extension=None):
+        if ctx.author.id == 643072638075273248:
+            if types is not None and extension is not None:
+                if types.lower() in folder_list:
+                    if extension == "*":
+                        for filenames in os.listdir(f'./{types}'):
+                            try:
+                                if filenames.endswith('.py'):
+                                    print(f"Load {filenames[:-3]}, by {ctx.author}")
+                                    self.client.load_extension(f'{types}.{filenames[:-3]}')
+                            except Exception:
+                                pass
+                        await ctx.send(f"Loaded all extensions in {types}, succes!")
+                    else:
+                        self.client.load_extension(f'{types}.{extension}')
+                        print(f"Load {extension}, by {ctx.author} in {types}")
+                        await ctx.send(f"Load {extension}, succes!")
+
+
+def setup(client):
+    client.add_cog(CoreLoad(client))
