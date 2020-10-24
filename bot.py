60b32d111e42c3a686c1a5ace599226318ed397a
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-24 21:22:43 +0200

    update bot to discord.py 1.5.1

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ import asyncio
 import os
 import discord
 from discord.ext import commands
-from data import settings
+from settings import token, bot_name, folder_list, prefix
 
-client = commands.Bot(command_prefix='b!', case_insensitive=True)
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix=prefix, case_insensitive=True, intents=intents)
 client.remove_command("help")
-
-folder_list = ['events', 'commands', 'moderator']
-bot_name = "Bumblebee"
 
 
 async def change_status():
@@ -17,59 +17,6 @@ async def change_status():
         status = discord.Activity(name=f"Optimus Prime", type=discord.ActivityType.watching)
         await client.change_presence(activity=status)
         await asyncio.sleep(60)
-
-
-@client.command()
-async def load(ctx, types=None, extension=None):
-    if ctx.author.id == 643072638075273248:
-        if types is not None and extension is not None:
-            if types.lower() in folder_list:
-                if extension == "*":
-                    for filenames in os.listdir(f'./{types}'):
-                        if filenames.endswith('.py'):
-                            print(f"Load {extension}, by {ctx.author}")
-                            client.load_extension(f'{types}.{filenames[:-3]}')
-                    await ctx.send(f"Loaded all extensions in {types}, succes!")
-                else:
-                    client.load_extension(f'{types}.{extension}')
-                    print(f"Load {extension}, by {ctx.author} in {types}")
-                    await ctx.send(f"Load {extension}, succes!")
-
-
-@client.command()
-async def reload(ctx, types=None, extension=None):
-    if ctx.author.id == 643072638075273248:
-        if types is not None and extension is not None:
-            if types.lower() in folder_list:
-                if extension == "*":
-                    for filenames in os.listdir(f'./{types}'):
-                        if filenames.endswith('.py'):
-                            print(f"Reload {extension}, by {ctx.author}")
-                            client.unload_extension(f'{types}.{filenames[:-3]}')
-                            client.load_extension(f'{types}.{filenames[:-3]}')
-                    await ctx.send(f"Reloaded all extensions in {types}, succes!")
-                else:
-                    client.unload_extension(f'{types}.{extension}')
-                    client.load_extension(f'{types}.{extension}')
-                    print(f"Reload {extension}, door {ctx.author} in {types}")
-                    await ctx.send(f"Reload {extension}, succes!")
-
-
-@client.command()
-async def unload(ctx, types=None, extension=None):
-    if ctx.author.id == 643072638075273248:
-        if types is not None and extension is not None:
-            if types.lower() in folder_list:
-                if extension == "*":
-                    for filenames in os.listdir(f'./{types}'):
-                        if filenames.endswith('.py'):
-                            print(f"Unload {extension}, by {ctx.author}")
-                            client.unload_extension(f'{types}.{filenames[:-3]}')
-                    await ctx.send(f"Unloaded all extensions in {types}, succes!")
-                else:
-                    client.unload_extension(f'{types}.{extension}')
-                    print(f"Unload {extension}, door {ctx.author} in {types}")
-                    await ctx.send(f"Unload {extension}, succes!")
 
 
 for folder in folder_list:
@@ -82,4 +29,4 @@ for folder in folder_list:
 print(f"[{bot_name}] ------------------------------------------------------")
 
 client.loop.create_task(change_status())
-client.run(settings.token)
+client.run(token)
