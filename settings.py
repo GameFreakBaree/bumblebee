a3d254b8a86e96eaf6613f37c86881ec8ff3b8f0
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-24 21:29:11 +0200

    Create Empty settings.py

---
+++ settings.py
@@ -0,0 +1,15 @@
+embedcolor = 0xFEBF08
+succes_emote = '✅'
+error_emote = '❌'
+footer = "© 2020 - Bumblebee"
+prefix = 'b!'
+
+bot_name = "Bumblebee"
+folder_list = ['core', 'events', 'commands', 'moderator']
+
+host = "IP:PORT"
+user = "db_user1"
+database = "bumblebee"
+passwd = "YOUR-PASSWORD-HERE"
+
+token = "YOUR-TOKEN-HERE"
