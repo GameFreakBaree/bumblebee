8a39a1558c176b82801cad97096b080183ac43b7
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-10-24 21:25:22 +0200

    Create unload.py

---
+++ core/unload.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+from settings import folder_list
+import os
+
+
+class CoreUnload(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def unload(self, ctx, types=None, extension=None):
+        if ctx.author.id == 643072638075273248:
+            if types is not None and extension is not None:
+                if types.lower() in folder_list:
+                    if extension == "*":
+                        for filenames in os.listdir(f'./{types}'):
+                            try:
+                                if filenames.endswith('.py'):
+                                    print(f"Unload {filenames[:-3]}, by {ctx.author}")
+                                    self.client.unload_extension(f'{types}.{filenames[:-3]}')
+                            except Exception:
+                                pass
+                        await ctx.send(f"Unloaded all extensions in {types}, succes!")
+                    else:
+                        self.client.unload_extension(f'{types}.{extension}')
+                        print(f"Unload {extension}, by {ctx.author} in {types}")
+                        await ctx.send(f"Unload {extension}, succes!")
+
+
+def setup(client):
+    client.add_cog(CoreUnload(client))
