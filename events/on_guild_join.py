16ba1e4ad1ddea1baa6422378e8eac1a4b87b585
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-12-07 20:12:47 +0100

    Create on_guild_join.py

---
+++ events/on_guild_join.py
@@ -0,0 +1,26 @@
+from discord.ext import commands
+import mysql.connector
+from settings import host, user, passwd, database
+
+
+class OnGuildJoin(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        print(f"[ADD] Guild: {guild} heeft de bot toegevoegd.")
+
+        insert_guilddata = "INSERT INTO bumblebee_guildsettings (guild_id, logchannel_id, mute_role) VALUES (%s, %s, %s)"
+        record = (guild.id, 0, 0)
+
+        db_bumblebee = mysql.connector.connect(host=host, database=database, user=user, passwd=passwd)
+        bumblebee_cursor = db_bumblebee.cursor()
+        bumblebee_cursor.execute(insert_guilddata, record)
+        db_bumblebee.commit()
+        db_bumblebee.close()
+
+
+def setup(client):
+    client.add_cog(OnGuildJoin(client))
