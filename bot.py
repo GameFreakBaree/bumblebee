95ba32c2c2ac206a8a2e36c21d705a9c56408b63
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-12-08 18:55:50 +0100

    added change_prefix function

--- bot.py
+++ bot.py
@@ -2,19 +2,38 @@ import asyncio
 import os
 import discord
 from discord.ext import commands
-from settings import token, bot_name, folder_list, prefix
+from settings import host, user, passwd, database, token, bot_name, folder_list
+import mysql.connector
 
 intents = discord.Intents.default()
 intents.members = True
 
-client = commands.Bot(command_prefix=prefix, case_insensitive=True, intents=intents)
+
+def get_prefix(client, message):
+    db_bumblebee = mysql.connector.connect(host=host, user=user, passwd=passwd, database=database)
+    bumblebee_cursor = db_bumblebee.cursor()
+
+    bumblebee_cursor.execute("SELECT prefix FROM bumblebee_guildsettings WHERE guild_id = %s" % (message.guild.id,))
+    prefix_tuple = bumblebee_cursor.fetchone()
+
+    bumblebee_cursor.close()
+
+    if prefix_tuple is None:
+        prefix = "b!"
+    else:
+        prefix = prefix_tuple[0]
+
+    return prefix
+
+
+client = commands.Bot(command_prefix=get_prefix, case_insensitive=True, intents=intents)
 client.remove_command("help")
 
 
 async def change_status():
     await client.wait_until_ready()
     while client.is_ready():
-        status = discord.Activity(name=f"Optimus Prime", type=discord.ActivityType.watching)
+        status = discord.Activity(name="ALPHA", type=discord.ActivityType.watching)
         await client.change_presence(activity=status)
         await asyncio.sleep(300)
 
@@ -23,8 +42,6 @@ async def change_status():
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         pass
-    else:
-        raise error
 
 for folder in folder_list:
     print(f"[{bot_name}] ----------------------[ {folder.title()} ]--------------------")
