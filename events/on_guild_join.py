c7f2a4197b951677124aa0eba077c62fd6582abf
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-12-08 18:57:07 +0100

    added prefix in database

--- events/on_guild_join.py
+++ events/on_guild_join.py
@@ -12,8 +12,8 @@ class OnGuildJoin(commands.Cog):
     async def on_guild_join(self, guild):
         print(f"[ADD] Guild: {guild} heeft de bot toegevoegd.")
 
-        insert_guilddata = "INSERT INTO bumblebee_guildsettings (guild_id, logchannel_id, mute_role) VALUES (%s, %s, %s)"
-        record = (guild.id, 0, 0)
+        insert_guilddata = "INSERT INTO bumblebee_guildsettings (guild_id, logchannel_id, mute_role, prefix) VALUES (%s, %s, %s, %s)"
+        record = (guild.id, 0, 0, "b!")
 
         db_bumblebee = mysql.connector.connect(host=host, database=database, user=user, passwd=passwd)
         bumblebee_cursor = db_bumblebee.cursor()
