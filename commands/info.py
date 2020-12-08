86178cd4c511661a6de39f405b95af2938677254
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-12-08 18:59:19 +0100

    Update info.py

--- commands/info.py
+++ commands/info.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
-import mysql.connector
-from settings import host, user, passwd, database, embedcolor
+from settings import embedcolor, footer
 
 
 class InfoCmd(commands.Cog):
@@ -11,19 +10,17 @@ class InfoCmd(commands.Cog):
 
     @commands.command()
     async def info(self, ctx):
-        db_bumblebee = mysql.connector.connect(host=host, database=database, user=user, passwd=passwd)
-        bumblebee_cursor = db_bumblebee.cursor()
-
-        bumblebee_cursor.execute(f"SELECT footer FROM bumblebee_guildsettings WHERE guild_id = {ctx.guild.id}")
-        embed_footer = bumblebee_cursor.fetchone()
-        db_bumblebee.close()
-
         embed = discord.Embed(
-            description=f"__Created by:__ GameFreakBaree#9999\n__Shard:__ 1/1\n\n**Useful URLs**\n[Invite the Bot](https://discord.com/oauth2/authorize?client_id=742776969481158766&permissions=1074097233&redirect_uri=https%3A%2F%2Fdiscord.gg%2F5gvn5pn&scope=bot) - [Wiki](https://github.com/GameFreakBaree/Bumblebee/wiki) - [Support Server](https://discord.gg/5gvn5pn)",
+            description=f"__Created by:__ GameFreakBaree#9999"
+                        f"\n__Shard:__ 1/1"
+                        f"\n\n**Useful URLs**"
+                        f"\n[Invite the Bot](https://discord.com/api/oauth2/authorize?client_id=744123497214902312&permissions=268823622&redirect_uri=https%3A%2F%2Fdiscord.gg%2F5gvn5pn&scope=bot)"
+                        f" - [Wiki](https://github.com/GameFreakBaree/Bumblebee/wiki)"
+                        f" - [Support Server](https://discord.gg/5gvn5pn)",
             color=embedcolor
         )
         embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
-        embed.set_footer(text=embed_footer[0])
+        embed.set_footer(text=footer)
         await ctx.send(embed=embed)
 
 
