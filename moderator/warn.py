725556c4e7f0ea7916d4b07035710cc7a1def827
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-12-12 12:52:05 +0100

    Update warn.py

--- moderator/warn.py
+++ moderator/warn.py
@@ -27,53 +27,61 @@ class ModWarn(commands.Cog):
 
         if command_status == 1:
             if member is not None and member != ctx.author:
-                bumblebee_cursor.execute(f"SELECT * FROM bumblebee_warns WHERE guild_id = {ctx.guild.id} AND user = {member.id}")
-                total_warns = bumblebee_cursor.fetchall()
+                if not member.bot:
+                    bumblebee_cursor.execute(f"SELECT * FROM bumblebee_warns WHERE guild_id = {ctx.guild.id} AND user = {member.id}")
+                    total_warns = bumblebee_cursor.fetchall()
 
-                if len(total_warns) <= 15:
-                    await member.send(f"You have been warned in **{ctx.guild}** by **{ctx.author}**\n```\nReason: {reason}\n```")
+                    if len(total_warns) <= 15:
+                        await member.send(f"You have been warned in **{ctx.guild}** by **{ctx.author}**\n```\nReason: {reason}\n```")
 
-                    prewarn_embed = discord.Embed(
-                        description=f"**Reason:** {reason}",
-                        color=embedcolor
-                    )
-                    prewarn_embed.set_author(name=f"{member} has been warned!", icon_url=member.avatar_url)
-                    await ctx.send(embed=prewarn_embed)
+                        prewarn_embed = discord.Embed(
+                            description=f"**Reason:** {reason}",
+                            color=embedcolor
+                        )
+                        prewarn_embed.set_author(name=f"{member} has been warned!", icon_url=member.avatar_url)
+                        await ctx.send(embed=prewarn_embed)
 
-                    instertwarn = "INSERT INTO bumblebee_warns (guild_id, user, moderator, reason) VALUES (%s, %s, %s, %s)"
-                    warn = (ctx.guild.id, member.id, ctx.author.id, reason)
-                    bumblebee_cursor.execute(instertwarn, warn)
-                    db_bumblebee.commit()
-
-                    bumblebee_cursor.execute(f"SELECT logchannel_id FROM bumblebee_guildsettings WHERE guild_id = {ctx.guild.id}")
-                    log_channel_id = bumblebee_cursor.fetchone()
-
-                    if log_channel_id is None:
-                        insert_guilddata = "INSERT INTO bumblebee_guildsettings (guild_id, logchannel_id, mute_role, prefix) VALUES (%s, %s, %s, %s)"
-                        record = (ctx.guild.id, 0, 0, "b!")
-                        bumblebee_cursor.execute(insert_guilddata, record)
+                        instertwarn = "INSERT INTO bumblebee_warns (guild_id, user, moderator, reason) VALUES (%s, %s, %s, %s)"
+                        warn = (ctx.guild.id, member.id, ctx.author.id, reason)
+                        bumblebee_cursor.execute(instertwarn, warn)
                         db_bumblebee.commit()
-                        log_channel_id = (0,)
 
-                    try:
-                        if log_channel_id[0] != 0:
-                            log_channel = self.client.get_channel(log_channel_id[0])
-                            warn_embed = discord.Embed(
-                                color=embedcolor,
-                                timestamp=datetime.datetime.utcnow()
-                            )
-                            warn_embed.add_field(name="User", value=f"**Name:**\t{member.mention}\n**ID:**\t{member.id}",
-                                                 inline=True)
-                            warn_embed.add_field(name="Moderator",
-                                                 value=f"**Name:**\t{ctx.author.mention}\n**ID:**\t{ctx.author.id}", inline=True)
-                            warn_embed.add_field(name="Reason", value=f"{reason}", inline=False)
-                            warn_embed.set_author(name=f"[WARN] {member}", icon_url=member.avatar_url)
-                            warn_embed.set_footer(text=footer)
-                            await log_channel.send(embed=warn_embed)
-                    except AttributeError:
-                        pass
+                        bumblebee_cursor.execute(f"SELECT logchannel_id FROM bumblebee_guildsettings WHERE guild_id = {ctx.guild.id}")
+                        log_channel_id = bumblebee_cursor.fetchone()
+
+                        if log_channel_id is None:
+                            insert_guilddata = "INSERT INTO bumblebee_guildsettings (guild_id, logchannel_id, mute_role, prefix) VALUES (%s, %s, %s, %s)"
+                            record = (ctx.guild.id, 0, 0, "b!")
+                            bumblebee_cursor.execute(insert_guilddata, record)
+                            db_bumblebee.commit()
+                            log_channel_id = (0,)
+
+                        try:
+                            if log_channel_id[0] != 0:
+                                log_channel = self.client.get_channel(log_channel_id[0])
+                                warn_embed = discord.Embed(
+                                    color=embedcolor,
+                                    timestamp=datetime.datetime.utcnow()
+                                )
+                                warn_embed.add_field(name="User", value=f"**Name:**\t{member.mention}\n**ID:**\t{member.id}",
+                                                     inline=True)
+                                warn_embed.add_field(name="Moderator",
+                                                     value=f"**Name:**\t{ctx.author.mention}\n**ID:**\t{ctx.author.id}", inline=True)
+                                warn_embed.add_field(name="Reason", value=f"{reason}", inline=False)
+                                warn_embed.set_author(name=f"[WARN] {member}", icon_url=member.avatar_url)
+                                warn_embed.set_footer(text=footer)
+                                await log_channel.send(embed=warn_embed)
+                        except AttributeError:
+                            pass
+                    else:
+                        await ctx.send("You reached the limit of warns you can give one user. **Buy PRO to unlock unlimited warns per user.**")
                 else:
-                    await ctx.send("You reached the limit of warns you can give one user. **Buy PRO to unlock unlimited warns per user.**")
+                    await ctx.send("You cannot warn a bot.")
+            else:
+                if member is None:
+                    await ctx.send("Invalid user.")
+                elif member == ctx.author:
+                    await ctx.send("You can't warn yourself.")
         db_bumblebee.close()
 
     @commands.command()
@@ -182,7 +190,7 @@ class ModWarn(commands.Cog):
     async def resetwarns(self, ctx):
         db_bumblebee = mysql.connector.connect(host=host, user=user, passwd=passwd, database=database)
         bumblebee_cursor = db_bumblebee.cursor()
-        bumblebee_cursor.execute(f"DELETE FROM bumblebee_warns WHERE guild_id = %s", ctx.guild.id)
+        bumblebee_cursor.execute(f"DELETE FROM bumblebee_warns WHERE guild_id = %s", (ctx.guild.id,))
         db_bumblebee.commit()
         db_bumblebee.close()
 
@@ -194,7 +202,9 @@ class ModWarn(commands.Cog):
         await ctx.send(embed=resetwarn_embed)
 
     @commands.command(aliases=["warns", "warnsinfo"])
+    @commands.has_permissions(manage_guild=True)
     async def warninfo(self, ctx, *, member: discord.Member):
+        # Command is not finished yet. Do not use this!
         db_bumblebee = mysql.connector.connect(host=host, database=database, user=user, passwd=passwd)
         bumblebee_cursor = db_bumblebee.cursor()
 
@@ -208,12 +218,13 @@ class ModWarn(commands.Cog):
 
         if command_status == 1:
             page = 1
-            offset = (page - 1) * 5
+            items_per_pagina = 3
+            offset = (page - 1) * items_per_pagina
             volgnummer = offset
             warn_list = ""
             doorgaan = True
 
-            bumblebee_cursor.execute(f"SELECT * FROM bumblebee_warns WHERE guild_id = {ctx.guild.id} AND user = {member.id} LIMIT 5")
+            bumblebee_cursor.execute(f"SELECT * FROM bumblebee_warns WHERE guild_id = {ctx.guild.id} AND user = {member.id} LIMIT {items_per_pagina}")
             warn_id_tuple = bumblebee_cursor.fetchall()
 
             for ids in warn_id_tuple:
@@ -238,10 +249,10 @@ class ModWarn(commands.Cog):
             max_rows_tuple = bumblebee_cursor.fetchall()
             max_rows = len(max_rows_tuple)
 
-            if max_rows % 5 != 0:
-                max_rows = max_rows // 5 + 1
+            if max_rows % items_per_pagina != 0:
+                max_rows = max_rows // items_per_pagina + 1
             else:
-                max_rows = max_rows // 5
+                max_rows = max_rows // items_per_pagina
 
             if max_rows == 1:
                 doorgaan = False
@@ -253,69 +264,7 @@ class ModWarn(commands.Cog):
             )
             warn_embed.set_author(name=f"[WARNINGS] {member}", icon_url=member.avatar_url)
             warn_embed.set_footer(text=f"{footer} | Page {page}/{max_rows}")
-            warn_message = await ctx.send(embed=warn_embed)
-
-            if doorgaan:
-                await warn_message.add_reaction("◀️")
-                await warn_message.add_reaction("▶️")
-
-                def check(reactie, gebruiker):
-                    return gebruiker == ctx.author and str(reactie.emoji) in ["◀️", "▶️"]
-
-                while True:
-                    try:
-                        reaction, member = await self.client.wait_for("reaction_add", timeout=30, check=check)
-
-                        if str(reaction.emoji) == "▶️" and page != max_rows:
-                            page += 1
-                            await warn_message.remove_reaction(reaction, member)
-                            nieuwe_pagina = True
-                        elif str(reaction.emoji) == "◀️" and page > 1:
-                            page -= 1
-                            await warn_message.remove_reaction(reaction, member)
-                            nieuwe_pagina = True
-                        else:
-                            await warn_message.remove_reaction(reaction, member)
-                            nieuwe_pagina = False
-
-                        if nieuwe_pagina:
-                            offset = (page - 1) * 3
-                            warn_list = ""
-                            volgnummer = offset
-
-                            bumblebee_cursor.execute(f"SELECT * FROM bumblebee_warns WHERE guild_id = {ctx.guild.id} AND user = {member.id} LIMIT {offset}, 5")
-                            warn_id_tuple = bumblebee_cursor.fetchall()
-
-                            for ids in warn_id_tuple:
-                                try:
-                                    user_name = self.client.get_user(ids[2])
-                                except AttributeError:
-                                    user_name = ids[2]
-
-                                try:
-                                    moderator = self.client.get_user(ids[3])
-                                except AttributeError:
-                                    moderator = ids[3]
-
-                                volgnummer += 1
-                                warn_list = f"{warn_list}**{volgnummer}.** **Warn ID:** {ids[0]}\n- **User:** {user_name}\n- **Moderator:** {moderator}\n- **Reason:** {ids[4]}\n\n"
-
-                            if warn_list == "":
-                                warn_list = "**No warnings found!**"
-
-                            warn_embed = discord.Embed(
-                                description=f"{warn_list}",
-                                color=embedcolor,
-                                timestamp=datetime.datetime.utcnow()
-                            )
-                            warn_embed.set_author(name=f"[WARNINGS] {member}", icon_url=member.avatar_url)
-                            warn_embed.set_footer(text=f"{footer} | Page {page}/{max_rows}")
-                            await warn_message.edit(embed=warn_embed)
-                    except asyncio.TimeoutError:
-                        await warn_message.clear_reaction("◀️")
-                        await warn_message.clear_reaction("▶️")
-                        break
-        db_bumblebee.close()
+            await ctx.send(embed=warn_embed)
 
     @warn.error
     async def warn_error(self, ctx, error):
@@ -326,6 +275,13 @@ class ModWarn(commands.Cog):
 
     @delwarn.error
     async def delwarn_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            pass
+        else:
+            raise error
+
+    @warninfo.error
+    async def warninfo_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             pass
         else:
