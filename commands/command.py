ee6521bff1e1dbb91f87d6e54e7a8459bdb6236a
Siebe Baree <33906209+GameFreakBaree@users.noreply.github.com>
2020-12-12 12:48:04 +0100

    Update command.py

--- commands/command.py
+++ commands/command.py
@@ -15,7 +15,7 @@ class CommandsCommand(commands.Cog):
         db_bumblebee = mysql.connector.connect(host=host, user=user, passwd=passwd, database=database)
         bumblebee_cursor = db_bumblebee.cursor()
 
-        bumblebee_cursor.execute("SELECT prefix FROM bumblebee_guildsettings WHERE guild_id = %s" % (ctx.guild.id,))
+        bumblebee_cursor.execute("SELECT prefix FROM bumblebee_guildsettings WHERE guild_id = %s", (ctx.guild.id,))
         prefix_tuple = bumblebee_cursor.fetchone()
 
         if prefix_tuple is None:
@@ -23,43 +23,46 @@ class CommandsCommand(commands.Cog):
         else:
             prefix = prefix_tuple[0]
 
-        command_list = ['ban', 'tempban', 'unban', 'mute', 'tempmute', 'unmute',
-                        'kick', 'clear', 'warn', 'automoderator']
+        command_list = ['ban', 'mute', 'kick', 'clear', 'warn']
 
-        if setting.lower() == "enable" or setting.lower() == "disable":
-            if setting.lower() == "enable":
-                on_off = 1
-            elif setting.lower() == "disable":
-                on_off = 0
-            else:
-                on_off = 0
+        if setting is not None:
+            if setting.lower() == "enable" or setting.lower() == "disable":
+                if setting.lower() == "enable":
+                    on_off = 1
+                elif setting.lower() == "disable":
+                    on_off = 0
+                else:
+                    on_off = 0
 
-            if command_name.lower() in command_list:
-                bumblebee_cursor.execute("SELECT * FROM bumblebee_commands WHERE guild_id = %s", ctx.guild.id)
-                command_data = bumblebee_cursor.fetchone()
+                if command_name.lower() in command_list:
+                    bumblebee_cursor.execute("SELECT * FROM bumblebee_commands WHERE guild_id = %s", (ctx.guild.id,))
+                    command_data = bumblebee_cursor.fetchone()
 
-                if command_data is None:
-                    insert_command_data = "INSERT INTO bumblebee_commands (guild_id, ban, mute, kick, clear, warn, automoderator) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                    record = (ctx.guild.id, 1, 1, 1, 1, 1, 1, 1)
-                    bumblebee_cursor.execute(insert_command_data, record)
+                    if command_data is None:
+                        insert_command_data = "INSERT INTO bumblebee_commands (guild_id, ban, mute, kick, clear, warn, automoderator) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                        record = (ctx.guild.id, 1, 1, 1, 1, 1, 1)
+                        bumblebee_cursor.execute(insert_command_data, record)
+                        db_bumblebee.commit()
+
+                    bumblebee_cursor.execute(f"UPDATE bumblebee_commands SET {command_name.lower()} = %s WHERE guild_id = %s", (on_off, ctx.guild.id))
                     db_bumblebee.commit()
 
-                bumblebee_cursor.execute(f"UPDATE bumblebee_commands SET {command_name.lower()} = %s WHERE guild_id = %s", (on_off, ctx.guild.id))
-                db_bumblebee.commit()
-
-                embed = discord.Embed(
-                    description=f"You've succesfully {setting.lower()}d the command {command_name.lower()}",
-                    color=embedcolor
-                )
-                embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
-                embed.set_footer(text=footer)
-                await ctx.send(embed=embed)
+                    embed = discord.Embed(
+                        description=f"You've succesfully {setting.lower()}d the command {command_name.lower()}",
+                        color=embedcolor
+                    )
+                    embed.set_author(name=self.client.user.display_name, icon_url=self.client.user.avatar_url)
+                    embed.set_footer(text=footer)
+                    await ctx.send(embed=embed)
+                else:
+                    await ctx.send(f"Invalid Arguments. Try: `{prefix}command <enable/disable> <command>`"
+                                   f"\nPossible Commands: ban, mute, kick, clear, warn")
             else:
                 await ctx.send(f"Invalid Arguments. Try: `{prefix}command <enable/disable> <command>`"
-                               f"\nPossible Commands: ban, tempban, unban, mute, tempmute, unmute, kick, clear, warn, automoderator")
+                               f"\nPossible Commands: ban, mute, kick, clear, warn")
         else:
             await ctx.send(f"Invalid Arguments. Try: `{prefix}command <enable/disable> <command>`"
-                           f"\nPossible Commands: ban, tempban, unban, mute, tempmute, unmute, kick, clear, warn, automoderator")
+                           f"\nPossible Commands: ban, mute, kick, clear, warn")
         db_bumblebee.close()
 
     @command.error
